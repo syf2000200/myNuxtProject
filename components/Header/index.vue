e8089cd2e9526741bb69eea7547e0b698ff7a6fc
sunyf <sunyf-c@glodon.com>
2018-08-16 15:32:45 +0800

    提交

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -1,5 +1,35 @@
 <template>
   <div class="Header">
-    Header
+    <div class="logo">logo</div>
+    <nav>
+      <ul>
+        <li><a href="http://">免费课程</a></li>
+        <li><a href="http://">职业路径</a></li>
+        <li><a href="http://">实战</a></li>
+        <li><a href="http://">猿问</a></li>
+        <li><a href="http://">手记</a></li>
+        <li><a href="http://">找工作</a></li>
+      </ul>
+    </nav>
+    <div class="search-wrap">
+      <div class="search-tags">
+        <a href="">前端入门</a>
+        <a href="">JAVA基础</a>
+      </div>
+      <div class="search-area">
+        <input type="text" class="search-input">
+      </div>
+      <div class="search-btn"></div>
+    </div>
+    <div class="login-area">
+      <ul>
+        <li><a href="http://">下载APP</a></li>
+        <li><a href="http://">购物车</a></li>
+        <li>
+          <a href="http://">登录</a>
+          <a href="http://">注册</a>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
