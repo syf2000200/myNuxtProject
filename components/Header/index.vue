d1f56eeb0ba5c62b26210d59129a06b30c18d2e2
sunyf <sunyf-c@glodon.com>
2018-08-20 08:55:56 +0800

    提交

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="Header">
-    <div class="logo">logo</div>
+    <HeaderLogo/>
     <nav>
       <ul>
         <li><a href="http://">免费课程</a></li>
@@ -11,7 +11,7 @@
         <li><a href="http://">找工作</a></li>
       </ul>
     </nav>
-    <div class="search-wrap">
+    <div class="search-warp">
       <div class="search-tags">
         <a href="">前端入门</a>
         <a href="">JAVA基础</a>
@@ -23,9 +23,9 @@
     </div>
     <div class="login-area">
       <ul>
-        <li><a href="http://">下载APP</a></li>
-        <li><a href="http://">购物车</a></li>
-        <li>
+        <li class="app-load"><a href="http://">下载APP</a></li>
+        <li class="shop-cart"><a href="http://">购物车</a></li>
+        <li class="header-signin">
           <a href="http://">登录</a>
           <a href="http://">注册</a>
         </li>
@@ -34,7 +34,15 @@
   </div>
 </template>
 
+
 <script>
+import HeaderLogo from '~/components/HeaderLogo'
 import './style.css'
+
+export default {
+  components: {
+    HeaderLogo
+  }
+}
 </script>
 
---
+++ components/HeaderLogo/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <div id="logo" class="logo"><img title="慕课网" src="~assets/images/index/logo.png"></div>
+</template>
