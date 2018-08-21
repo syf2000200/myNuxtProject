1f507b85a9a9a4cd852fc907399f1abfdff0fc8f
sunyf <sunyf-c@glodon.com>
2018-08-21 10:21:59 +0800

    提交

--- components/Banner/index.vue
+++ components/Banner/index.vue
@@ -1,55 +1,24 @@
 <template>
     <div class="bgfff banner-box">
         <div class="g-banner">
-          <div class="menuwrap"></div>
-          <div class="menuContent">
-            <div class="item">
-              <a href="">
-                <span class="group">前沿 / 区块链 / 人工智能</span>
-              </a>
-            </div>
-          </div>
-          <div class="g-banner-content">
-            <div class="g-banner-box">
-              <a href="">
-                <div class="banner-slide">
-                  <img src="~assets/images/index/banner01.jpg" alt="">
-                </div>
-              </a>
-            </div>
-            <div class="path-banner clearfix">
-              <a href="">
-                <i class="i-web"></i>
-                <p class="tit">Web前端攻城狮</p>
-                <p class="desc">互联网时代最火爆的技术</p>
-              </a>
-              <a href="">
-                <i class="i-java"></i>
-                <p class="tit">Java攻城狮</p>
-                <p class="desc">综合就业排名第一</p>
-              </a>
-              <a href="">
-                <i class="i-android"></i>
-                <p class="tit">Android攻城狮</p>
-                <p class="desc">移动设备市场份额第一</p>
-              </a>
-              <a href="">
-                <i class="i-php"></i>
-                <p class="tit">PHP攻城狮</p>
-                <p class="desc">世界上最好的语言：）</p>
-              </a>
-              <a href="">
-                <i class="i-ios"></i>
-                <p class="tit">iOS攻城狮</p>
-                <p class="desc">可能是全球最好用的系统</p>
-              </a>
-            </div>
-          </div>
+          <!-- MENU -->
+          <Menu/>
+          <!-- SLIDERBANNER -->
+          <SlideBanner/>
         </div>
       </div>
 </template>
 
 <script>
+    import Menu from '~/components/Menu'
+    import SlideBanner from '~/components/SlideBanner'
     import './style.css'
+
+    export default {
+      components: {
+        Menu,
+        SlideBanner
+      }
+    }
 </script>
 
---
+++ components/Menu/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="menuContent">
+        <div class="item">
+            <a href="">
+            <span class="group">前沿 / 区块链 / 人工智能</span>
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+    import './style.css'
+</script>
